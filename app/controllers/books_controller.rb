56ae56334fb1c3f702970621afa6468bfc0ea21a
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-08-21 16:19:18 +0900

    create_books_index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,29 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+  def updata
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
