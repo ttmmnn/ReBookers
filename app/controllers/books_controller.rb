13c31b2b99da4a63934ae4c337e58b0387ba76f4
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-08-21 17:40:43 +0900

    create_delete_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,12 +14,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-  def updata
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
